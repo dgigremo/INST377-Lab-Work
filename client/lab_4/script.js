dacffa2619cdcbcd076611834f3d1b56f08faac1
dgigremo <54863495+dgigremo@users.noreply.github.com>
2021-09-28 18:41:54 -0400

    Troubleshooting carousel.

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,5 +1,5 @@
 let slidePosition = 0;
-const slides = document.querySelectorAll('carousel_item');
+const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
 
 document.
