4f539094d3b9cc79eb3c620d0ac5acdfce5d09df
dgigremo <54863495+dgigremo@users.noreply.github.com>
2021-10-06 20:23:34 -0400

    Addressing errors in code.

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -49,49 +49,53 @@ document.addEventListener("DOMContentLoaded", () => {
     iTetromino,
   ];
 
-
-  let currentPosition = 4
-  let currentRotation = 0
+  let currentPosition = 4;
+  let currentRotation = 0;
 
   //randomly select a Tetromino and its first rotation
-  let random = Math.floor(Math.random()*theTetrominoes.length)
-  let current = theTetrominoes[random][currentRotation]
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[random][currentRotation];
 
   //draw theTetromino
   function draw() {
-      current.forEach(index => {
-          squares[currentPosition + index].classList.add('tetromino')
-      })
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino');
+    });
   }
 
   //undraw the Tetromino
   function undraw() {
-      current.forEach(index => {
-          squares[currentPosition + index].classList.remove('tetromino')
-      })
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino');
+    });
   }
 
   //make the tetromino move down every second
-  timerId = setInterval(moveDown, 1000)
+  timerId = setInterval(moveDown, 1000);
 
   //move down function
-  function moveDown () {
-    undraw()
-    currentPosition += width
-    draw()
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
   }
 
   //freeze function
   function freeze() {
-      if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-          current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-          //start a new tetromino falling
-          random = Math.floor(Math.random() * theTetrominoes.length)
-          current = theTetrominoes[raandom][currentRotation]
-          currentPosition = 4
-          draw()
-      }
+    if (
+      current.some(index =>
+        squares[currentPosition + index + width].classList.contains('taken')
+      )
+    ) {
+      current.forEach(index =>
+        squares[currentPosition + index].classList.add('taken')
+      );
+      //start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
   }
-
-
-})
+});
