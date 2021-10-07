8c4420f04a9d5a52dcda64537c0dbcd733f78c73
dgigremo <54863495+dgigremo@users.noreply.github.com>
2021-10-06 23:21:57 -0400

    Added gameOver function.

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -121,6 +121,7 @@ document.addEventListener("DOMContentLoaded", () => {
       draw()
       displayShape()
       addScore()
+      gameOver()
     }
   }
 
@@ -224,10 +225,21 @@ function addScore() {
             scoreDisplay.innerHTML = score
             row.forEach(index => {
                 squares[index].classList.remove('taken')
+                squares[index].classList.remove('tetromino')
             })
             const squaresRemoved = squares.splice(i, width)
-            console.log(squaresRemoved)
+            squares = squaresRemoved.concat(squares)
+            squares.forEach(cell => grid.appendChild(cell))
         }
+    }
+}
+
+
+//game over
+function gameOver() {
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        scoreDisplay.innerHTML = 'end'
+        clearInterval(timerId)
     }
 }
 
