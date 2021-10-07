f24e8407d0fed81898c8fd3a842d15a6d706b08b
dgigremo <54863495+dgigremo@users.noreply.github.com>
2021-10-06 23:06:19 -0400

    Added mini-grid that displays next tetromino shape.

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const ScoreDisplay = document.querySelector("#score")
   const StartBtn = document.querySelector("#start-button")
   const width = 10
+  let nextRandom = 0
 
   //The Tetrominoes
   const lTetromino = [
@@ -106,10 +107,12 @@ document.addEventListener("DOMContentLoaded", () => {
         squares[currentPosition + index].classList.add("taken")
       )
       //start a new tetromino falling
-      random = Math.floor(Math.random() * theTetrominoes.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
       currentPosition = 4
       draw()
+      displayShape()
     }
   }
 
@@ -161,7 +164,30 @@ function rotate() {
 }
 
 
+//show up-next tetromino in mini-grid display
+const displaySquares = document.querySelectorAll('.mini-grid div')
+const displayWidth = 4
+let displayIndex = 0
 
+//the Tetrominoes without rotations
+const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+    [0, 1, displayWidth, displayWidth+1], //oTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+]
+
+//display the shape in the mini-grid display
+function displayShape() {
+    //remove any trace of a tetromino from the entire grid
+    displaySquares.forEach(square => {
+        square.classList.rmeove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach( index => {
+        displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+}
 
 
 })
