149a2783052b6c5423cec8cc0cf23e443f305cba
dgigremo <54863495+dgigremo@users.noreply.github.com>
2022-02-18 16:19:49 -0500

    Tidying up code

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -10,42 +10,41 @@ document.addEventListener('DOMContentLoaded', () => {
   const colors = ['orange', 'red', 'purple', 'green', 'blue']
 
 
-  //The Tetrominoes
   // The Tetrominoes
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2]
-  ];
+  ]
  
   const zTetromino = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1]
-  ];
+  ]
  
   const tTetromino = [
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1]
-  ];
+  ]
  
   const oTetromino = [
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1]
-  ];
+  ]
  
   const iTetromino = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3]
-  ];
+  ]
  
   const theTetrominoes = [
     lTetromino,
@@ -53,14 +52,14 @@ document.addEventListener('DOMContentLoaded', () => {
     tTetromino,
     oTetromino,
     iTetromino
-  ];
+  ]
 
   let currentPosition = 4
   let currentRotation = 0
 
   //randomly select a Tetromino and its first rotation
-  let random = Math.floor(Math.random()*theTetrominoes.length)
-  let current = theTetrominoes[random][0]
+  let random = Math.floor(Math.random() * theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
 
   //draw the tetromino
   function draw() {
@@ -78,9 +77,6 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  //make the Tetromino move down every second
-  //timerId = setInterval(moveDown, 1000)
-
   //assign functions to keyCodes
   function control(e) {
     if(e.keyCode === 37) {
@@ -93,6 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
       moveDown()
     }
   }
+  document.addEventListener('keyup', control)
 
   //move down function
   function() {
